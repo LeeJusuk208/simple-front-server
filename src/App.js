53e0737466d27f1f9d9de10c8c575cad01f90132
root <root@DESKTOP-2LNIRDJ>
2023-08-31 00:04:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const params = JSON.parse(urlParams.get('userResponse'));
+  console.log(params);
+
+  let LoginElement;
+  if(params !== null && params.status === "Login Success"){
+    window.localStorage.setItem('userResponse', params)
+    LoginElement = () => {
+      return (
+        <p>{params.email}</p>
+      );
+    };
+  } else {
+    LoginElement = () => {
+      return (
+        <div>
+          <a href="http://musinsa-price-tracker.com/api/oauth/login/google?redirect_url=http://musinsa-price-tracker.com">
+            <button>Google Login</button>
+          </a> <br/>
+          
+          <a href="http://musinsa-price-tracker.com/api/oauth/login/kakao?redirect_url=http://musinsa-price-tracker.com">
+            <button>Kakao Login</button>
+          </a> <br/>
+          
+          <a href="http://musinsa-price-tracker.com/api/oauth/login/naver?redirect_url=http://musinsa-price-tracker.com">
+            <button>Naver Login</button>
+          </a> <br/>
+        </div>
+      );
+    };
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginElement/>
     </div>
   );
 }
